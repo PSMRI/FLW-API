db06877c5248afb4d7854fe044b9d8572bb1899b
kalyan <kalyan@beehyv.com>
2023-08-02 14:49:58 +0530

    updated get query in cbac create date

--- src/main/java/com/iemr/flw/repo/identity/CbacRepo.java
+++ src/main/java/com/iemr/flw/repo/identity/CbacRepo.java
@@ -15,6 +15,6 @@ public interface CbacRepo extends JpaRepository<CbacDetails, Long> {
     @Query(value = "SELECT cbac FROM CbacDetails cbac WHERE cbac.createdBy = :user and cbac.createdDate >= :fromDate and cbac.createdDate <= :toDate")
     List<CbacDetails> getAllByCreatedBy(@Param("user") String user, @Param("fromDate") Timestamp fromDate, @Param("toDate") Timestamp toDate);
 
-    @Query(value = "Select * from db_identity.i_cbacdetails where beneficiaryRegId = :benRegId and DATE(createdDate) = DATE(:createdDate) limit 1", nativeQuery = true)
+    @Query(value = "Select * from db_identity.i_cbacdetails where beneficiaryRegId = :benRegId and createdDate = :createdDate limit 1", nativeQuery = true)
     CbacDetails findCbacDetailsByBeneficiaryRegIdAndCreatedDate(@Param("benRegId") Long benRegId, @Param("createdDate") Timestamp createdDate);
 }
