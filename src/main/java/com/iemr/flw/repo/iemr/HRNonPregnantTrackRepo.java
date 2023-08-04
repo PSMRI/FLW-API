700a3a61211b6c9ac5d5a7deb5967a499b4bb5dd
kalyan <kalyan@beehyv.com>
2023-08-04 21:44:46 +0530

    visit param in hrp api

--- src/main/java/com/iemr/flw/repo/iemr/HRNonPregnantTrackRepo.java
+++ src/main/java/com/iemr/flw/repo/iemr/HRNonPregnantTrackRepo.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface HRNonPregnantTrackRepo extends JpaRepository<NonPregnantWomanHighRiskTrack, Long> {
 
     @Query(value = "SELECT npt FROM NonPregnantWomanHighRiskTrack npt WHERE npt.benId = :benId and npt.userId = :userId and npt.visitDate = :visitDate")
-    NonPregnantWomanHighRiskTrack getByUserIdAndBenId(@Param("benId") Long benId, @Param("userId") Integer userId, Timestamp visitDate);
+    NonPregnantWomanHighRiskTrack getByUserIdAndBenId(@Param("benId") Long benId, @Param("userId") Integer userId, @Param("visitDate") Timestamp visitDate);
 
     @Query(value = "SELECT npt FROM NonPregnantWomanHighRiskTrack npt WHERE npt.userId = :userId and npt.visitDate >= :fromDate and npt.visitDate <= :toDate")
     List<NonPregnantWomanHighRiskTrack> getByUserId(@Param("userId") Integer userId, @Param("fromDate") Timestamp fromDate, @Param("toDate") Timestamp toDate);
--- src/main/java/com/iemr/flw/repo/iemr/HRPregnantTrackRepo.java
+++ src/main/java/com/iemr/flw/repo/iemr/HRPregnantTrackRepo.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface HRPregnantTrackRepo extends JpaRepository<PregnantWomanHighRiskTrack, Long> {
 
     @Query(value = "SELECT hrpt FROM PregnantWomanHighRiskTrack hrpt WHERE hrpt.benId = :benId and hrpt.userId = :userId and hrpt.visit = :visit")
-    PregnantWomanHighRiskTrack getByUserIdAndBenId(@Param("benId") Long benId, @Param("userId") Integer userId, String visit);
+    PregnantWomanHighRiskTrack getByUserIdAndBenId(@Param("benId") Long benId, @Param("userId") Integer userId, @Param("visit") String visit);
 
     @Query(value = "SELECT hrpt FROM PregnantWomanHighRiskTrack hrpt WHERE hrpt.userId = :userId and hrpt.visitDate >= :fromDate and hrpt.visitDate <= :toDate")
     List<PregnantWomanHighRiskTrack> getByUserId(@Param("userId") Integer userId, @Param("fromDate") Timestamp fromDate, @Param("toDate") Timestamp toDate);
