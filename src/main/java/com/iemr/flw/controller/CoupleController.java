7ec501b265e24e2dfbd47f4338efebf9a209ea88
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2025-02-24 10:41:29 +0530

    Coderabbit comments addressed

--- src/main/java/com/iemr/flw/controller/CoupleController.java
+++ src/main/java/com/iemr/flw/controller/CoupleController.java
@@ -1,30 +1,32 @@
 package com.iemr.flw.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.iemr.flw.dto.identity.GetBenRequestHandler;
 import com.iemr.flw.dto.iemr.EligibleCoupleDTO;
 import com.iemr.flw.dto.iemr.EligibleCoupleTrackingDTO;
 import com.iemr.flw.service.CoupleService;
-import com.iemr.flw.utils.ApiResponse;
 import com.iemr.flw.utils.response.OutputResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/couple", headers = "Authorization")
 public class CoupleController {
 
-
+	private final Gson gson = new GsonBuilder().setDateFormat("MMM dd, yyyy HH:mm:ss a").create();
     private final Logger logger = LoggerFactory.getLogger(CoupleController.class);
 
     @Autowired
@@ -98,7 +100,6 @@ public class CoupleController {
             logger.error("Error in fetching eligible couple registration details, " + e);
             response.setError(5000, "Error in fetching eligible couple registration details : " + e);
         }
-       Gson gson = new GsonBuilder().setDateFormat("MMM dd, yyyy HH:mm:ss a").create();
        return gson.toJson(response);
     }
 
