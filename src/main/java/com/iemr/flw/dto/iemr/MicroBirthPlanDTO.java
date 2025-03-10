047fa0135147447cc26b85ba1f9481a975336904
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-03-10 15:17:37 +0530

    fix code issue in asha profile

--- src/main/java/com/iemr/flw/dto/iemr/MicroBirthPlanDTO.java
+++ src/main/java/com/iemr/flw/dto/iemr/MicroBirthPlanDTO.java
@@ -3,9 +3,10 @@ package com.iemr.flw.dto.iemr;
 import com.iemr.flw.domain.iemr.MicroBirthPlan;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 @Data
-public class MicroBirthPlanDTO  {
+public class MicroBirthPlanDTO implements Serializable {
     Integer  userId;
     List<MicroBirthPlan> entries;
 }
