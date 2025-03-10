e231025815dc2d9005bb07aa60ae6cdb4e9144f6
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-03-10 14:43:32 +0530

    Disease control module

--- src/main/java/com/iemr/flw/repo/iemr/MicroBirthPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iemr.flw.repo.iemr;
-
-import com.iemr.flw.domain.iemr.MicroBirthPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MicroBirthPlanRepository extends JpaRepository<MicroBirthPlan, Long> {
-}
