704dbc8643922feac17535774ebb52c48f504b5a
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-03-11 18:05:34 +0530

    fixed code

--- src/main/java/com/iemr/flw/service/impl/EmployeeMasterImpl.java
+++ src/main/java/com/iemr/flw/service/impl/EmployeeMasterImpl.java
@@ -3,16 +3,29 @@ package com.iemr.flw.service.impl;
 import com.iemr.flw.domain.iemr.M_User;
 import com.iemr.flw.repo.iemr.EmployeeMasterRepo;
 import com.iemr.flw.service.EmployeeMasterInter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmployeeMasterImpl  implements EmployeeMasterInter {
+public class EmployeeMasterImpl implements EmployeeMasterInter {
     @Autowired
-    EmployeeMasterRepo  employeeMasterRepo;
+    EmployeeMasterRepo employeeMasterRepo;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
 
     @Override
     public M_User getUserDetails(Integer userID) {
-        return employeeMasterRepo.findByUserID(userID);
+        logger.debug("Fetching user details for userID: {}", userID);
+        try {
+            if (userID == null) {
+                throw new IllegalArgumentException("UserID cannot be null");
+            }
+            return employeeMasterRepo.findByUserID(userID);
+        } catch (Exception e) {
+            logger.error("Error fetching user details for userID: {}", userID, e);
+            throw e;
+        }
     }
 }
