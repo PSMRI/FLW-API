03e351abde26f8a6e426e049e483bad22a22642e
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-04-30 14:08:43 +0530

    IRS round

--- src/main/java/com/iemr/flw/controller/IRSRoundController.java
+++ src/main/java/com/iemr/flw/controller/IRSRoundController.java
@@ -6,11 +6,15 @@ import com.iemr.flw.dto.iemr.IRSRoundListDTO;
 import com.iemr.flw.service.IRSRoundService;
 import com.iemr.flw.utils.response.OutputResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Method;
 import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/irsRound", headers = "Authorization")
@@ -19,7 +23,7 @@ public class IRSRoundController {
     @Autowired
     private IRSRoundService irsRoundService;
 
-    @RequestMapping(name = "/add", method = RequestMethod.POST)
+    @RequestMapping(name = "/add", method = RequestMethod.POST, headers = "Authorization")
     public String addRound(@RequestBody IRSRoundListDTO dto) {
         OutputResponse response = new OutputResponse();
         try {
@@ -42,9 +46,24 @@ public class IRSRoundController {
 
     }
 
-    @RequestMapping(name = "/list/{householdId}", method = RequestMethod.GET)
-    public List<IRSRound> getRounds(@PathVariable Long householdId) {
-        return irsRoundService.getRounds(householdId);
+    @RequestMapping(name = "/list/{householdId}", method = RequestMethod.GET, headers = "Authorization")
+    public ResponseEntity<Map<String, Object>> getRounds(@PathVariable Long householdId) {
+
+        Map<String, Object> response = new LinkedHashMap<>();
+
+        try {
+            Map<String, Object> data = new HashMap<>();
+            data.put("entries", irsRoundService.getRounds(householdId));
+            response.put("data", data);
+            response.put("statusCode", 200);
+            response.put("message", "Success");
+        } catch (Exception e) {
+            response.put("statusCode", 500);
+            response.put("errorMessage", e.getMessage());
+
+        }
+
+        return ResponseEntity.ok(response);
     }
 
 
