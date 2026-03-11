6725306ba0a774ac413e81419f0e9bef65b54452
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-03-11 16:36:16 +0530

    ssd portal api job

--- src/main/java/com/iemr/flw/dto/identity/GetBenRequestHandler.java
+++ src/main/java/com/iemr/flw/dto/identity/GetBenRequestHandler.java
@@ -14,6 +14,26 @@
 
 		private Long activityId;
 
+		private Integer month;
+
+		private Integer year;
+
+		public Integer getMonth() {
+			return month;
+		}
+
+		public void setMonth(Integer month) {
+			this.month = month;
+		}
+
+		public Integer getYear() {
+			return year;
+		}
+
+		public void setYear(Integer year) {
+			this.year = year;
+		}
+
 		public Long getActivityId() {
 			return activityId;
 		}
