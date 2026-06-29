544e9ba7407a9127bd327d232612d75b13718fa7
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-06-29 19:37:38 +0530

    fix hbnc incentive

--- src/main/java/com/iemr/flw/service/impl/MaternalHealthServiceImpl.java
+++ src/main/java/com/iemr/flw/service/impl/MaternalHealthServiceImpl.java
@@ -670,18 +670,22 @@ public class MaternalHealthServiceImpl implements MaternalHealthService {
             if (maleSterilizationActivityCH != null) {
                 addIncenticeRecord(ect, userId, maleSterilizationActivityCH);
             }
-
-            if (ect.getPncPeriod() == 42) {
-
+            if(ect.getMotherDangerSign() != null
+                    && !ect.getMotherDangerSign().isEmpty()
+                    && ect.getPncPeriod() == 42){
                 IncentiveActivity highRiskPostpartumCareActivityCH =
                         incentivesRepo.findIncentiveMasterByNameAndGroup("HIGH_RISK_POSTPARTUM_CARE", GroupName.ACTIVITY.getDisplayName());
-
-                IncentiveActivity highRiskPostpartumHealthCareActivityCH =
-                        incentivesRepo.findIncentiveMasterByNameAndGroup("HIGH_RISK_POSTPARTUM_HEALTH_CHECK", GroupName.ACTIVITY.getDisplayName());
                 if (highRiskPostpartumCareActivityCH != null) {
                     addIncenticeRecord(ect, userId, highRiskPostpartumCareActivityCH);
 
                 }
+
+            }
+            if (ect.getPncPeriod() == 42) {
+
+
+                IncentiveActivity highRiskPostpartumHealthCareActivityCH =
+                        incentivesRepo.findIncentiveMasterByNameAndGroup("HIGH_RISK_POSTPARTUM_HEALTH_CHECK", GroupName.ACTIVITY.getDisplayName());
 
                 if (highRiskPostpartumHealthCareActivityCH != null) {
                     addIncenticeRecord(ect, userId, highRiskPostpartumHealthCareActivityCH);
