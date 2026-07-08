d1ce6dbfcf1e591f4b40c86e8b012c16cb13ea62
Saurav Mishra <80103738+SauravBizbRolly@users.noreply.github.com>
2026-07-08 16:30:38 +0530

    Fix/pb mb incentive issue 3.8.2 (#271)
    * fix  duplicate incentive issue
    
    * fix  duplicate incentive issue
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix child after 2 year gap after marriage
    
    * fix hbyc incentive logic
    
    * fix hbyc incentive logic
    
    * fix hbyc incentive logic
    
    * optmiz query for incentive logic for hbnc

--- src/main/java/com/iemr/flw/repo/identity/BeneficiaryRepo.java
+++ src/main/java/com/iemr/flw/repo/identity/BeneficiaryRepo.java
@@ -26,7 +26,7 @@ public interface BeneficiaryRepo extends JpaRepository<RMNCHBeneficiaryDetailsRm
     Optional<RMNCHBeneficiaryDetailsRmnch> findById(Long benID);
 
     List<RMNCHBeneficiaryDetailsRmnch> findByHouseoldId(Long houseHoldId);
-    Optional<RMNCHBeneficiaryDetailsRmnch> findByBenficieryid(Long benID);
+    List<RMNCHBeneficiaryDetailsRmnch> findByBenficieryid(Long benID);
 
     List<RMNCHBeneficiaryDetailsRmnch> findByHouseoldId(Long houseHoldId);
     List<RMNCHBeneficiaryDetailsRmnch> findByBenficieryid(Long benID);
