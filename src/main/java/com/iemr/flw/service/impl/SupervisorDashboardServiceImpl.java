1764b86f30c2b390bcf4786a457fb4ae69bfaca3
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-07-29 14:42:20 +0530

    changes logic for cg incentive

--- src/main/java/com/iemr/flw/service/impl/SupervisorDashboardServiceImpl.java
+++ src/main/java/com/iemr/flw/service/impl/SupervisorDashboardServiceImpl.java
@@ -658,7 +658,10 @@ public class SupervisorDashboardServiceImpl implements SupervisorDashboardServic
             if (!activityList.isEmpty()) {
                 approvalStatus = (int) activityList.get(0).get("approvalStatus");
             }
-            if (totalAmount == null || totalAmount <= 0) continue;
+            if(!approvalStatusID.equals(106)){
+                if (totalAmount == null || totalAmount <= 0) continue;
+
+            }
 
             if (pending == 0 && verified == 0 && rejected == 0 && unclaimedCount == 0) continue;
             if (approvalStatusID.equals(0)) {
