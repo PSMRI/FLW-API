13c0909036a233a34928e003c690b7596619cb7c
sehjotsinghunthinkable <sehjot.singh@unthinkable.co>
2026-08-13 12:09:56 +0530

    Feature/duplicate order update (#321)
    * Fixing issue for creation of multiple duplicate order
    
    * Fixing issue for creation of multiple duplicate order
    
    * Fixing issue for creation of multiple duplicate order
    
    * Updating the flow for saving tb_suspected
    
    * Updating the flow for saving tb_suspected
    
    ---------
    
    Co-authored-by: Saurav Mishra <80103738+SauravBizbRolly@users.noreply.github.com>

--- src/main/java/com/iemr/flw/repo/iemr/TBSuspectedRepo.java
+++ src/main/java/com/iemr/flw/repo/iemr/TBSuspectedRepo.java
@@ -37,4 +37,5 @@ public interface TBSuspectedRepo extends JpaRepository<TBSuspected, Long> {
     @Modifying
     @Query("UPDATE TBSuspected t SET t.vanSerialNo = t.id WHERE t.id = :id")
     void updateVanSerialNo(@Param("id") Long id);
+
 }
