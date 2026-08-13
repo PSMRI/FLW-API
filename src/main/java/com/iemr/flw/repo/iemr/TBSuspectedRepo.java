9c23c5da4abdd64c8694e52ab341eb63ba2ba8be
Sehjot Singh Pannu <sehjot.singh@unthinkable.co>
2026-08-13 12:05:30 +0530

    Updating the flow for saving tb_suspected

--- src/main/java/com/iemr/flw/repo/iemr/TBSuspectedRepo.java
+++ src/main/java/com/iemr/flw/repo/iemr/TBSuspectedRepo.java
@@ -38,5 +38,4 @@ public interface TBSuspectedRepo extends JpaRepository<TBSuspected, Long> {
     @Query("UPDATE TBSuspected t SET t.vanSerialNo = t.id WHERE t.id = :id")
     void updateVanSerialNo(@Param("id") Long id);
 
-    TBSuspected findFirstByBenIdOrderByCreatedDateDesc(Long beneficiaryId);
 }
