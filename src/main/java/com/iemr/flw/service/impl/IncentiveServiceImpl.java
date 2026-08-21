bfb582c9b40f745a8e44925f50d39febb93c493b
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-08-21 12:39:33 +0530

    add group cat name

--- src/main/java/com/iemr/flw/service/impl/IncentiveServiceImpl.java
+++ src/main/java/com/iemr/flw/service/impl/IncentiveServiceImpl.java
@@ -525,7 +525,7 @@ public class IncentiveServiceImpl implements IncentiveService {
                     incentivesRepo.findById(request.getActivityId()).orElse(null);
 
             String groupName = activity != null ? activity.getGroup() : "";
-            String groupCategoryName = activity != null ? activity.getGroupCategoryName() :activity.getGroup();
+//            String groupCategoryName = activity != null ? activity.getGroupCategoryName() :activity.getGroup();
             String description = activity != null ? activity.getDescription() : "";
 
             // 🔹 Map result
@@ -539,8 +539,9 @@ public class IncentiveServiceImpl implements IncentiveService {
                     }
                 }
 
-                r.setGroupName(groupCategoryName);
+                r.setGroupName(groupName);
                 r.setActivityDec(description);
+//                r.setGroupCategoryName(groupCategoryName);
 
                 return modelMapper.map(r, IncentiveRecordDTO.class);
 
